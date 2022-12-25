b7774586c5656bf86b75bd9661689c2b51f26098
NomanAli42856 <121042827+NomanAli42856@users.noreply.github.com>
2022-12-25 20:01:14 +0500

    adding files

---
+++ task01.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+main ()
+{
+char ch;
+
+cout << "Enter a special character to print pyramid > ";
+cin >> ch;
+
+cout << ch << endl;
+cout << ch << ch << endl;
+cout << ch << ch  << ch << endl;
+cout << ch << ch  << ch  << ch << endl;
+cout << ch << ch  << ch  << ch  << ch << endl;
+cout << ch << ch  << ch  << ch  << ch  << ch << endl;
+cout << ch << ch  << ch  << ch  << ch  << ch  << ch << endl;
+cout << ch << ch  << ch  << ch  << ch  << ch  << ch << ch;
+
+}
